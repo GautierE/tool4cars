862fd0a01d1d5a8b35545b7ea2d596218de1a45a
Gautier <gautier.edel.info@gmail.com>
2023-07-25 10:43:42 +0200

    feat: conditional color on cars for client C

--- customs/clienta/modules/cars/ajax.php
+++ customs/clienta/modules/cars/ajax.php
@@ -6,6 +6,15 @@ $clientCars = array_filter($cars, function ($car) {
     return $car["customer"] === "clienta";
 });
 ?>
+<style>
+    .older-than-10-years {
+        background-color: red;
+    }
+
+    .younger-than-2-years {
+        background-color: green;
+    }
+</style>
 
 <h1>Client A cars</h1>
 
@@ -21,7 +30,21 @@ $clientCars = array_filter($cars, function ($car) {
         </thead>
         <tbody>
             <?php foreach ($clientCars as $car) { ?>
-                <tr data-car-id=<?php echo $car["id"]; ?>>
+                <?php
+                $currentYear = date("Y");
+                $carYear = date("Y", $car["year"]);
+                $isOlderThan10Years = ($currentYear - $carYear) > 10;
+                $isYoungerThan2Years = ($currentYear - $carYear) < 2;
+                $cssClass = "";
+
+                if ($isOlderThan10Years) {
+                    $cssClass = "older-than-10-years";
+                } elseif ($isYoungerThan2Years) {
+                    $cssClass = "younger-than-2-years";
+                }
+                ?>
+
+                <tr data-car-id=<?php echo $car["id"]; ?> class="<?php echo $cssClass; ?>">
                     <td><?php echo $car["modelName"]; ?></td>
                     <td><?php echo $car["brand"]; ?></td>
                     <td><?php echo date("Y", $car["year"]); ?></td>
